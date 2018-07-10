65d03e00a7f4a0ec05152e30fcce6df4cef3a85d
Your Name <johnmuchirim@gmail.com>
2018-07-10 11:51:38 +0300

    intial  commit

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-
-
+import NavBar from '../../features/nav/NavBar/NavBar' 
+import {Container} from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 class App extends Component {
   render() {
     return (
       <div>
-       <h1> Revent</h1>
+      
+        <NavBar/>
+
+   <Container className="main">
+        <EventDashboard/>
+        </Container>
+
       </div>
       
     );
